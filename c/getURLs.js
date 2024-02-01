4d6f5cdd18d676518e3b086ce900bd0c7a20b3cb
Mingchao Zhang <mingchao_zhang@brown.edu>
2024-02-01 18:47:59 +0000

    getURLs done

--- c/getURLs.js
+++ c/getURLs.js
@@ -5,7 +5,11 @@ const { JSDOM } = require('jsdom');
 const { URL } = require('url');
 
 // Example usage:
-const baseURL = process.argv[2] + "/";
+let baseURL = process.argv[2] + "/";
+let indexLen = "index.html/".length
+if (baseURL.slice(-indexLen) === "index.html/") {
+  baseURL = baseURL.slice(0, -indexLen)
+}
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -14,7 +18,24 @@ const rl = readline.createInterface({
 // TODO some code
 
 rl.on('line', (line) => {
-  // TODO some code
+  // Process each line of the HTML stream
+  const dom = new JSDOM(line)
+  const document = dom.window.document
+  const anchors = document.querySelectorAll('a')
+
+  for (const anchor of anchors) {
+    const href = anchor.getAttribute('href')
+    if (href) {
+      // Check if href is a relative URL
+      const isRelativeURL = !/^https?:\/\//i.test(href)
+      // Resolve relative URLs relative to the baseURL
+      if (isRelativeURL) {
+        console.log(new URL(href, baseURL).href)
+      } else {
+        console.log(href)
+      }
+    }
+  }
 });
 
 rl.on('close', () => {
